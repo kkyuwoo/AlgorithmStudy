904d5683a1069f2d9406a5b37ea3517c8ebc1901
kkyuwoo <115778770+kkyuwoo@users.noreply.github.com>
2023-12-03 15:10:34 +0900

    [Silver V] Title: K번째 수, Time: 3936 ms, Memory: 623280 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.py
+++ 백준/Silver/11004. K번째 수/K번째 수.py
@@ -1,5 +1,55 @@
 import sys
 N, K = map(int, sys.stdin.readline().split())
 A = list(map(int, sys.stdin.readline().split()))
-A.sort()
-print(A[K-1])
+
+def quickSort(S, E, K):
+    global A
+    if S < E:
+        pivot = partition(S, E)
+        if pivot == K:
+            return
+        elif K < pivot:
+            quickSort(S, pivot - 1, K)
+        else:
+            quickSort(pivot + 1, E, K)
+
+
+def swap(i, j):
+    global A
+    temp = A[i]
+    A[i] = A[j]
+    A[j] = temp
+
+
+def partition(S, E):
+    global A
+
+    if S + 1 == E:
+        if A[S] > A[E]:
+            swap(S, E)
+        return E
+
+    M = (S + E) // 2
+    swap(S, M)
+    pivot = A[S]
+    i = S + 1
+    j = E
+    while i <= j:
+        while j >= S and pivot < A[j]:
+            j = j - 1
+        while  i <= E and pivot > A[i] :
+            i = i + 1
+        if i < j:
+            swap(i, j)
+            i = i + 1
+            j = j - 1
+        else:
+            break
+
+    A[S] = A[j]
+    A[j] = pivot
+    return j
+
+
+quickSort(0, N - 1, K - 1)
+print(A[K - 1])
