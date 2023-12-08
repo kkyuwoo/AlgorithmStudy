b6721cf05e7ff9f2f47e04a18b3d1c9765e632ab
kkyuwoo <115778770+kkyuwoo@users.noreply.github.com>
2023-12-08 14:18:59 +0900

    [Silver III] Title: 소수 구하기, Time: 408 ms, Memory: 69512 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,15 @@
+m, n = map(int, input().split())
+A = [0] * (n+1)
+
+for i in range(2, n+1):
+    A[i] = i
+
+for i in range(2, int(n**0.5)+1):
+    if A[i] == 0:
+        continue
+    for j in range(i+i, n+1, i):
+        A[j] = 0
+
+for i in range(m, n+1):
+    if A[i] != 0:
+        print(A[i])
