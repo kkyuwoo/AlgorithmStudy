8e022ecd98969928522e698e409461da6b62e88d
kkyuwoo <115778770+kkyuwoo@users.noreply.github.com>
2023-12-21 21:10:06 +0900

    [Silver III] Title: 1로 만들기, Time: 532 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,11 +1,11 @@
-x = int(input())
-d = [0] * (x+1)
+n = int(input())
+D = [0] * (n+1)
 
-for i in range(2, x+1):
-    d[i] = d[i-1] + 1
+for i in range(2, n+1):
+    D[i] = D[i-1] + 1
     if i % 2 == 0:
-        d[i] = min(d[i], d[i//2] + 1)
+        D[i] = min(D[i], D[i//2]+1)
     if i % 3 == 0:
-        d[i] = min(d[i], d[i//3] + 1)
+        D[i] = min(D[i], D[i//3]+1)
 
-print(d[x])
+print(D[n])
