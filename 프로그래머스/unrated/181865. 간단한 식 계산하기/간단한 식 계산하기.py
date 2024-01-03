b1eb3d9ac47a26b14db6a2fcbb01c66fbbe6ee68
kkyuwoo <115778770+kkyuwoo@users.noreply.github.com>
2024-01-03 21:54:10 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,11 @@
+def solution(binomial):
+    answer = 0
+    array = list(binomial.split())
+    if array[1] == '+':
+        answer = int(array[0]) + int(array[2])
+    elif array[1] == '-':
+        answer = int(array[0]) - int(array[2])
+    else:
+        answer = int(array[0]) * int(array[2])
+            
+    return answer
