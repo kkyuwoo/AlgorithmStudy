e24ebc414c9078df8608189b9b695cc5f22cc9de
kkyuwoo <115778770+kkyuwoo@users.noreply.github.com>
2024-01-16 19:16:50 +0900

    [level Lv.0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,22 @@
+def solution(code):
+    answer = ''
+    mode = 0
+    ret = ''
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i] != "1":
+                if i % 2 == 0:
+                    ret += code[i]
+            else:
+                mode = 1
+        elif mode == 1:
+            if code[i] != "1":
+                if i % 2 != 0:
+                    ret += code[i]
+            else:
+                mode = 0
+    if len(ret) != 0:
+        answer = ret
+    else:
+        answer = "EMPTY"
+    return answer
