5bc31cdc06c466528d2e71026578d58d8e663987
kkyuwoo <115778770+kkyuwoo@users.noreply.github.com>
2024-01-16 18:25:20 +0900

    [level Lv.0] Title: 그림 확대, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181836. 그림 확대/그림 확대.py
@@ -0,0 +1,9 @@
+def solution(picture, k):
+    answer = []
+    for i in picture:
+        temp = ''
+        for j in i:
+            temp += j*k
+        for _ in range(k):
+            answer.append(temp)
+    return answer
